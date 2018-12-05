fe7879aa754d9f6812084ebea825383a88d26926
Daniel D'Avella <drdavella@gmail.com>
2018-12-05 13:09:21 -0500

    Add test for installation with no conda_deps

--- tests/test_conda_env.py
+++ tests/test_conda_env.py
@@ -26,3 +26,31 @@ def test_conda_create(newconfig, mocksession):
     assert '-p' == pcalls[0].args[3]
     assert venv.path == pcalls[0].args[4]
     assert pcalls[0].args[5].startswith('python=')
+
+
+def test_install_deps_no_conda(newconfig, mocksession):
+    '''Test installation using conda when no conda_deps are given'''
+    config = newconfig(
+        [],
+        """
+        [testenv:py123]
+        deps=
+            numpy
+            astropy
+    """,
+    )
+
+    venv = VirtualEnv(config.envconfigs["py123"], session=mocksession)
+    action = mocksession.newaction(venv, "getenv")
+    tox_testenv_create(action=action, venv=venv)
+    pcalls = mocksession._pcalls
+    assert len(pcalls) == 1
+    pcalls[:] = []
+
+    assert len(venv.envconfig.deps) == 2
+    assert len(venv.envconfig.conda_deps) == 0
+
+    tox_testenv_install_deps(action=action, venv=venv)
+    assert len(pcalls) == 1
+    cmd = pcalls[0].args
+    assert cmd[1:4] == ['-m', 'pip', 'install']
