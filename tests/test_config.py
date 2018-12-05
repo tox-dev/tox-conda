8e47ab61b5fcd5537bf4390f52574c1ebac7e3dd
Daniel D'Avella <drdavella@gmail.com>
2018-12-05 12:27:15 -0500

    Add basic tests of tox-conda config fields

---
+++ tests/test_config.py
@@ -0,0 +1,77 @@
+def test_conda_deps(tmpdir, newconfig):
+    config = newconfig(
+        [],
+        """
+        [tox]
+        toxworkdir = {}
+        [testenv:py1]
+        deps=
+            hello
+        conda_deps=
+            world
+            something
+    """.format(
+            tmpdir
+        ),
+    )
+
+    assert len(config.envconfigs) == 1
+    assert hasattr(config.envconfigs['py1'], 'deps')
+    assert hasattr(config.envconfigs['py1'], 'conda_deps')
+    assert len(config.envconfigs['py1'].conda_deps) == 2
+    # For now, as a workaround, we temporarily add all conda dependencies to
+    # deps as well. This allows tox to know whether an environment needs to be
+    # updated or not. Eventually there may be a cleaner solution.
+    assert len(config.envconfigs['py1'].deps) == 3
+    assert 'world' in config.envconfigs['py1'].conda_deps
+    assert 'something' in config.envconfigs['py1'].conda_deps
+
+
+def test_no_conda_deps(tmpdir, newconfig):
+    config = newconfig(
+        [],
+        """
+        [tox]
+        toxworkdir = {}
+        [testenv:py1]
+        deps=
+            hello
+    """.format(
+            tmpdir
+        ),
+    )
+
+    assert len(config.envconfigs) == 1
+    assert hasattr(config.envconfigs['py1'], 'deps')
+    assert hasattr(config.envconfigs['py1'], 'conda_deps')
+    assert hasattr(config.envconfigs['py1'], 'conda_channels')
+    assert len(config.envconfigs['py1'].conda_deps) == 0
+    assert len(config.envconfigs['py1'].conda_channels) == 0
+    assert len(config.envconfigs['py1'].deps) == 1
+
+
+def test_conda_channels(tmpdir, newconfig):
+    config = newconfig(
+        [],
+        """
+        [tox]
+        toxworkdir = {}
+        [testenv:py1]
+        deps=
+            hello
+        conda_deps=
+            something
+            else
+        conda_channels=
+            conda-forge
+    """.format(
+            tmpdir
+        ),
+    )
+
+    assert len(config.envconfigs) == 1
+    assert hasattr(config.envconfigs['py1'], 'deps')
+    assert hasattr(config.envconfigs['py1'], 'conda_deps')
+    assert hasattr(config.envconfigs['py1'], 'conda_channels')
+    assert len(config.envconfigs['py1'].conda_channels) == 1
+    assert 'conda-forge' in config.envconfigs['py1'].conda_channels
