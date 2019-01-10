ba2561b0177cfd8c1a8a7470f8d96b2b74379d03
Daniel D'Avella <ddavella@stsci.edu>
2019-01-10 12:42:49 -0500

    Remove classifiers for Py2

--- setup.py
+++ setup.py
@@ -32,8 +32,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
