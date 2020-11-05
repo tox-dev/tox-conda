3330cb02861175291b1e76aba025897d683722ff
AntoineD <antoine.dechaume@protonmail.ch>
2020-11-05 17:02:29 +0100

    Fix python interpreter not correctly set after anaconda env creation.
    This both fixes:
    - failing env commands
    - env forced recreation the 2nd time
    - warnings from interpreter mismatch

--- tox_conda/plugin.py
+++ tox_conda/plugin.py
@@ -100,6 +100,12 @@ def tox_testenv_create(venv, action):
 
     venv.envconfig.conda_python = python
 
+    # let the venv know about the target interpreter just installed in our conda env, otherwise
+    # we'll have a mismatch later because tox expects the interpreter to be existing outside of
+    # the env
+    del venv.envconfig.config.interpreters.name2executable[venv.name]
+    venv.envconfig.config.interpreters.get_executable(venv.envconfig)
+
     return True
 
 
