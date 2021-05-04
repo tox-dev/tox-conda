dd1eaa9f87fd036a74c4c19b73271eaa4d4bf05d
Antoine Dechaume <AntoineD@users.noreply.github.com>
2021-05-04 09:33:27 +0200

    Install pypi dependencies and run all commands from activated anaconda envs (#71)
    Co-authored-by: Antoine DECHAUME <antoine.dechaume@irt-saintexupery.com>

--- tests/test_conda.py
+++ tests/test_conda.py
@@ -23,3 +23,34 @@ def test_conda(cmd, initproj):
     ), result.output()
 
     assert result.outlines[-4] == "True"
+
+
+def test_conda_run_command(cmd, initproj):
+    """Check that all the commands are run from an activated anaconda env.
+
+    This is done by looking at the CONDA_PREFIX environment variable which contains
+    the environment name.
+    This variable is dumped to a file because commands_{pre,post} do not redirect
+    their outputs.
+    """
+    env_name = "foobar"
+    initproj(
+        "pkg-1",
+        filedefs={
+            "tox.ini": """
+                [tox]
+                skipsdist=True
+                [testenv:{}]
+                commands_pre = python -c "import os; open('commands_pre', 'w').write(os.environ['CONDA_PREFIX'])"
+                commands = python -c "import os; open('commands', 'w').write(os.environ['CONDA_PREFIX'])"
+                commands_post = python -c "import os; open('commands_post', 'w').write(os.environ['CONDA_PREFIX'])"
+            """.format(  # noqa: E501
+                env_name
+            )
+        },
+    )
+    result = cmd("-v", "-e", env_name)
+    result.assert_success()
+
+    for filename in ("commands_pre", "commands_post", "commands"):
+        assert open(filename).read().endswith(env_name)
