7c49b4d25f6096b7005b8f27d035da0791d2a627
Emilio Gozo <15209351+emiliogozo@users.noreply.github.com>
2024-05-31 22:26:34 +0800

    test: use tox.pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,25 @@
-from pathlib import Path
 from types import TracebackType
-from typing import Any, Dict, Optional, Sequence
+from typing import Optional, Sequence
 
 import pytest
-from pytest import MonkeyPatch
-from pytest_mock import MockerFixture
 from tox.execute.api import ExecuteInstance, ExecuteOptions, ExecuteStatus
 from tox.execute.request import ExecuteRequest
 from tox.execute.stream import SyncWrite
-from tox.pytest import CaptureFixture, ToxProject, ToxProjectCreator
 
 from tox_conda.plugin import CondaEnvRunner
 
-
-@pytest.fixture(name="tox_project")
-def init_fixture(
-    tmp_path: Path,
-    capfd: CaptureFixture,
-    monkeypatch: MonkeyPatch,
-    mocker: MockerFixture,
-) -> ToxProjectCreator:
-    def _init(
-        files: Dict[str, Any], base: Optional[Path] = None, prj_path: Optional[Path] = None
-    ) -> ToxProject:
-        """create tox  projects"""
-        return ToxProject(files, base, prj_path or tmp_path / "p", capfd, monkeypatch, mocker)
-
-    return _init
+pytest_plugins = "tox.pytest"
 
 
 @pytest.fixture
 def mock_conda_env_runner(request, monkeypatch):
     class MockExecuteStatus(ExecuteStatus):
         def __init__(
-            self, options: ExecuteOptions, out: SyncWrite, err: SyncWrite, exit_code: int
+            self,
+            options: ExecuteOptions,
+            out: SyncWrite,
+            err: SyncWrite,
+            exit_code: int,
         ) -> None:
             super().__init__(options, out, err)
             self._exit_code = exit_code
